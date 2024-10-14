9e2b1832e9c6e09627db690f8d16e691d975ad66
timphilip12 <timothee.philip@gmail.com>
2024-10-13 19:58:04 -0500

    Merged all in one clean page

--- Ham_Radio_Project/src/main/java/edu/augustana/App.java
+++ Ham_Radio_Project/src/main/java/edu/augustana/App.java
@@ -17,7 +17,7 @@ public class App extends Application {
 
     @Override
     public void start(Stage stage) throws IOException {
-        scene = new Scene(loadFXML("EnglishToMorse"), 640, 480);
+        scene = new Scene(loadFXML("MainPage"), 640, 480);
         stage.setScene(scene);
         stage.setTitle("Morse Code Converter");
         stage.show();
---
+++ Ham_Radio_Project/src/main/java/edu/augustana/MainPageController.java
@@ -0,0 +1,217 @@
+package edu.augustana;
+
+import javafx.application.Platform;
+import javafx.fxml.FXML;
+import javafx.scene.control.*;
+import javafx.scene.layout.VBox;
+
+import javax.sound.sampled.LineUnavailableException;
+import java.util.ArrayList;
+
+public class MainPageController {
+
+
+    @FXML
+    private VBox morseMessagesVBox;
+    @FXML
+    private VBox englishMessagesVBox;
+    @FXML
+    private ScrollPane morseMessagesScrollPane;
+    @FXML
+    private ScrollPane englishMessagesScrollPane;
+    @FXML
+    private Label morseInput = new Label();
+    @FXML
+    private Slider frequencySlider;
+
+
+    private final ArrayList<String> frequency1Morse = new ArrayList<>();
+    private final ArrayList<String> frequency2Morse = new ArrayList<>();
+    private final ArrayList<String> frequency3Morse = new ArrayList<>();
+    private final ArrayList<String> frequency4Morse = new ArrayList<>();
+    private final ArrayList<String> frequency5Morse = new ArrayList<>();
+
+    private final ArrayList<String> frequency1English = new ArrayList<>();
+    private final ArrayList<String> frequency2English = new ArrayList<>();
+    private final ArrayList<String> frequency3English = new ArrayList<>();
+    private final ArrayList<String> frequency4English = new ArrayList<>();
+    private final ArrayList<String> frequency5English = new ArrayList<>();
+
+    private final MorseCodeConverter converter = new MorseCodeConverter();
+    private Boolean isTranslationHidden = true;
+
+    //Code from exam 1 (Chatter Box)
+    private void addMessageToChatLogUI(String message, VBox vbox, ScrollPane scrollpane) {
+        Label label = new Label("User:  " + message);
+        label.setWrapText(true);
+        vbox.getChildren().add(label);
+    }
+
+    @FXML
+    private void writeToFrequency() {
+        morseMessagesVBox.getChildren().clear();
+        String morseText = morseInput.getText();
+        String englishText = converter.MorseToEnglish(morseText);
+        int sliderValue = (int) frequencySlider.getValue();
+
+        addMessageToFrequency(sliderValue, morseText, englishText);
+
+        displayMorseMessagesFromFrequency(sliderValue);
+        morseInput.setText("");
+    }
+
+    @FXML
+    private void displayFrequency() {
+        morseMessagesVBox.getChildren().clear();
+        englishMessagesVBox.getChildren().clear();
+        int sliderValue = (int) frequencySlider.getValue();
+
+        displayMorseMessagesFromFrequency(sliderValue);
+        if (!isTranslationHidden){
+            displayEnglishMessagesFromFrequency(sliderValue);
+        }
+    }
+
+    @FXML
+    private void showTranslation(){
+        englishMessagesVBox.getChildren().clear();
+        int sliderValue = (int) frequencySlider.getValue();
+        displayEnglishMessagesFromFrequency(sliderValue);
+        isTranslationHidden = false;
+    }
+
+    @FXML
+    private void hideTranslation(){
+        englishMessagesVBox.getChildren().clear();
+        isTranslationHidden = true;
+    }
+
+    @FXML
+    private void playSound(){
+        int sliderValue = (int) frequencySlider.getValue();
+
+        ArrayList<String> morseTextList = getFrequencyEnglishList(sliderValue);
+        for (String morseText: morseTextList){
+            try {
+                SoundProducer.ProduceSound(morseText);
+            } catch (LineUnavailableException e){
+                e.printStackTrace();
+            }
+        }
+    }
+
+    @FXML
+    private void ditButton(){
+        morseInput.setText(morseInput.getText() + ".");
+
+        try {
+            SoundProducer.ProduceSound("e");
+        } catch (LineUnavailableException e){
+            e.printStackTrace();
+        }
+    }
+
+    @FXML
+    private void dahButton(){
+        morseInput.setText(morseInput.getText() + "-");
+
+        try {
+            SoundProducer.ProduceSound("t");
+        } catch (LineUnavailableException e){
+            e.printStackTrace();
+        }
+    }
+
+    @FXML
+    private void charSpaceButton(){
+        morseInput.setText(morseInput.getText() + " ");
+    }
+
+    @FXML
+    private void wordSpaceButton(){
+        morseInput.setText(morseInput.getText() + "   ");
+
+    }
+
+
+    private void addMessageToFrequency(int sliderValue, String morseText, String englishText) {
+        switch (sliderValue) {
+            case 1:
+                frequency1Morse.add(morseText);
+                frequency1English.add(englishText);
+                break;
+            case 2:
+                frequency2Morse.add(morseText);
+                frequency2English.add(englishText);
+                break;
+            case 3:
+                frequency3Morse.add(morseText);
+                frequency3English.add(englishText);
+                break;
+            case 4:
+                frequency4Morse.add(morseText);
+                frequency4English.add(englishText);
+                break;
+            case 5:
+                frequency5Morse.add(morseText);
+                frequency5English.add(englishText);
+                break;
+            default:
+                break;
+        }
+    }
+
+    private void displayMorseMessagesFromFrequency(int sliderValue) {
+        ArrayList<String> frequencyMorse = getFrequencyMorseList(sliderValue);
+
+        if (frequencyMorse != null) {
+            for (String message : frequencyMorse) {
+                addMessageToChatLogUI(message, morseMessagesVBox, morseMessagesScrollPane);
+            }
+        }
+    }
+
+    private void displayEnglishMessagesFromFrequency(int sliderValue) {
+        ArrayList<String> frequencyEnglish = getFrequencyEnglishList(sliderValue);
+
+        if (frequencyEnglish != null) {
+            for (String message : frequencyEnglish) {
+                addMessageToChatLogUI(message, englishMessagesVBox, englishMessagesScrollPane);
+            }
+        }
+    }
+
+    private ArrayList<String> getFrequencyMorseList(int sliderValue) {
+        switch (sliderValue) {
+            case 1:
+                return frequency1Morse;
+            case 2:
+                return frequency2Morse;
+            case 3:
+                return frequency3Morse;
+            case 4:
+                return frequency4Morse;
+            case 5:
+                return frequency5Morse;
+            default:
+                return null;
+        }
+    }
+
+    private ArrayList<String> getFrequencyEnglishList(int sliderValue) {
+        switch (sliderValue) {
+            case 1:
+                return frequency1English;
+            case 2:
+                return frequency2English;
+            case 3:
+                return frequency3English;
+            case 4:
+                return frequency4English;
+            case 5:
+                return frequency5English;
+            default:
+                return null;
+        }
+    }
+}
